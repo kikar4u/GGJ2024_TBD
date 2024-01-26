b11de58a1724bd3ff82c19ca0b5fe9f8c823dc67
Sylve Renault <sylve.renault@yahoo.fr>
2024-01-26 23:17:14 +0100

    feat : Plushie controller
    Player can jump towards mouse direction, jump is triggered only around a specific radius from the mesh

---
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private bool isJumping = false;
+    public GameObject test;
+    [Range(0f, 10f)]
+    [SerializeField] float jumpPower = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        // Check if the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Get the mouse position in screen coordinates
+            Vector3 mousePosition = Input.mousePosition;
+
+            // Create a ray from the camera through the mouse position
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            // Declare a RaycastHit variable to store information about the hit
+            RaycastHit hit;
+
+            // Check if the ray hits something in the scene
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.DrawRay(Camera.main.ScreenPointToRay(mousePosition).origin, hit.point, Color.yellow, 1000f);
+                if (hit.collider.gameObject.name == "Player")
+                {
+                    Jump(hit.point);
+                }
+            }
+        }
+    }
+    public void Jump(Vector3 hitPosition)
+    {
+        Vector3 jumpDirection = hitPosition - transform.position;
+        jumpDirection.Normalize();
+
+        // Apply a force to make the object jump
+        playerRb.velocity = jumpDirection * jumpPower;
+
+        // Limit the maximum speed
+        playerRb.velocity = Vector3.ClampMagnitude(playerRb.velocity, 5);
+    }
+}
